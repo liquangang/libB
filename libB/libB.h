49bf6a4088e226c1fa00344e5b192c81823a9342
liquangang <liquangang@idstaff.com>
2017-04-10 12:10:44 +0800

    提交项目代码

---
+++ libB/libB.h
@@ -0,0 +1,19 @@
+//
+//  libB.h
+//  libB
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libB.
+FOUNDATION_EXPORT double libBVersionNumber;
+
+//! Project version string for libB.
+FOUNDATION_EXPORT const unsigned char libBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libB/PublicHeader.h>
+
+
